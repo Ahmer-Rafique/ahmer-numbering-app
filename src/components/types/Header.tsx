17b4fba5ed790fb761920e04a0582128ec961d2b
AhmerAli17 <ahmerali1771@gmail.com>
2023-08-14 19:20:53 +0100

    spel seting

--- src/components/types/Header.tsx
+++ src/components/types/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
         <h1 className='flex justify-center mt-11 text-5xl text-red-500 hover:text-green-400 '>
-            Quater 2
+            Quarter 2
         </h1>
     </div>
   )
