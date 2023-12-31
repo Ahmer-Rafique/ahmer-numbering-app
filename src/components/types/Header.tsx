3625e46eb6af14e29711f2a782eca83f955880ad
AhmerAli17 <ahmerali1771@gmail.com>
2023-12-31 15:34:11 +0000

    update

--- src/components/types/Header.tsx
+++ src/components/types/Header.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
+      <div>
         <h1 className='flex justify-center mt-11 text-5xl text-red-500 hover:text-green-400 '>
             Quarter 2
         </h1>
+        </div>
     </div>
   )
 }
